60ae9256f68b26003a6e3b96ebbac2f34becebcb
K.Sato <s.kentecakademy@gmail.com>
2021-01-23 15:10:21 +0000

    CRUD kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,64 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+    
+  def show
+    @task = Task.find(params[:id])
+  end
+    
+  def new
+    @task = Task.new
+  end
+    
+  def create
+    @task = Task.new(task_params)
+        
+    if @task.save
+      flash[:success] = 'タスクが正常に登録されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが登録されませんでした'
+      render :new
+    end
+  end
+  
+    
+    
+  def edit
+    @task = Task.find(params[:id])
+  end
+    
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'タスクが編集されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが編集されませんでした'
+      render :new
+    end
+  end
+    
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_to tasks_url
+  end
+
+    
+    
+  private
+    
+  def set_task
+    @task = Task.find(params[:id])
+  end
+    
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
