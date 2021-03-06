131c73c53445c81baca88d971510bd7eb27faf3e
K.Sato <s.kentecakademy@gmail.com>
2021-03-06 10:33:37 +0000

    kadai-tasklist-user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,54 +1,54 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all
+    @task = Task.order(created_at: :desc).page(params[:page]).per(3)
   end
-    
+  
   def show
   end
-    
+  
   def new
     @task = Task.new
   end
-    
+  
   def create
     @task = Task.new(task_params)
-        
     if @task.save
-      flash[:success] = 'タスクが正常に登録されました'
+      flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'タスクが登録されませんでした'
-      render :new
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render new
     end
   end
-    
+  
   def edit
   end
-    
+  
   def update
     if @task.update(task_params)
-      flash[:success] = 'タスクが編集されました'
+      flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'タスクが編集されませんでした'
-      render :new
+      flash.now[:danger] = 'タスクは更新されませんでした'
+      render :edit
     end
   end
-    
+  
+
   def destroy
     @task.destroy
-
-    flash[:success] = 'タスクは正常に削除されました'
+    flash[:success] = 'タスクを削除しました。'
     redirect_to tasks_url
   end
-    
+  
   private
   
   def set_task
     @task = Task.find(params[:id])
   end
-    
+  
   def task_params
     params.require(:task).permit(:content, :status)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :new, :create]
   
+  resources :tasks
 end
